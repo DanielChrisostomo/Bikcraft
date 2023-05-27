7ced512e1fa801fe402254d811585bf41d4e6b9c
DanielChrisostomo <daniluz1515@gmail.com>
2023-05-27 16:58:34 -0300

    perguntas frequentes js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,23 @@ function AtivarProduto(parametro) {
 }
 
 parametros.forEach(AtivarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
