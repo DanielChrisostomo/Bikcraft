c5a68bda851bc4446b83eb0d6d4c23f2f12fcd3d
DanielChrisostomo <daniluz1515@gmail.com>
2023-05-28 16:18:55 -0300

    galeria imagens

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ const parametros = new URLSearchParams(location.search);
 function AtivarProduto(parametro) {
   const elemento = document.getElementById(parametro);
   elemento.checked = true;
-  console.log(elemento);
 }
 
 parametros.forEach(AtivarProduto);
@@ -42,3 +41,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
